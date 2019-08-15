ae3d3bf3a6a9412ba3417347d95c403d00d72bbd
mojtaba.ashena <mojtaba.ashena@yahoo.com>
2019-08-15 07:59:42 +0430

    Station class Added

---
+++ TankMonitorV1/Models/Station.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TankMonitorV1.Models
+{
+    public class Station
+    {
+        public int Id { get; set; }
+
+        public int GSID { get; set; }
+
+        public string GSName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Location { get; set; }
+
+
+    }
+}
---
+++ TankMonitorV1/Models/Tank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TankMonitorV1.Models
+{
+    public class Tank
+    {
+        public int Id { get; set; }
+
+        public int SerialNumber { get; set; }
+
+        public int Capasity { get; set; }
+
+
+    }
+}
